4248e0c7dad14fef48a712f224b6cf63020147b8
gabriruf <grufinosouza@tutanota.com>
2025-02-21 11:00:20 -0300

    mais exercicios

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# Exercício n°0
 def calcNota():
     nota1 = int(input("Insira primeira nota: ")) 
     nota2 = int(input("Insira segunda nota: ")) 
     media = (nota1 + nota2) / 2
     print(f"Média final: {media}")
 
+# Exercicio n°1
 def nome():
     nome = input("Digite seu nome: ")
     curso = input("Digite o curso: ")
@@ -18,4 +20,52 @@ def nome():
     print(f"Disciplina: {disci}\n")
     print(f"{produto}")
 
-nome()
+# Exercicio n°2
+def cidade():
+    nome = input("Nome: ")
+    idade = int(input("Idade: "))
+    cidade = input("Cidade: ")
+
+    print(f"Nome: {nome}\nIdade: {idade}\nCidade: {cidade}")
+
+# Exercicio n°3
+def atributos():
+    altura = 1.80
+    peso = 73.55
+
+    print(f"{peso}\n{altura}")
+
+# Exercicio n°4
+def programa4():
+    idade = 18
+    ano = 2002
+
+# Exercicio n°5
+def declarVarComplex():
+    a = 5+2j
+    b = 20+6j
+
+    print(a)
+    print(b)
+
+# Exercicio n°6
+def declarVarString():
+    nome = 'Guilherme'
+    profissao = 'Engenheiro de Software'
+
+    print(f"{profissao}\n{nome}")
+
+# Exercicio n°7
+def declarVarBool():
+    fim_de_semana = False
+    feriado = True 
+
+    print(f"{fim_de_semana}\n{feriado}")
+    
+# Exercicio n°8
+def declarVarList():
+    alunos = ['Amanda', 'Ana', 'Bruno', 'João']
+    notas = [10, 8.5, 7.8, 8.0]
+
+    print(f"{alunos}\n{notas}")
+
