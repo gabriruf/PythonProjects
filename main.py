b9734036aea2927115370fab0c401f095c586c2b
gabriruf <grufinosouza@tutanota.com>
2025-02-26 17:15:01 -0300

    atualizando codigo com exercicios novos

--- main.py
+++ main.py
@@ -1,71 +1,95 @@
 # Exercício n°0
-def calcNota():
-    nota1 = int(input("Insira primeira nota: ")) 
-    nota2 = int(input("Insira segunda nota: ")) 
-    media = (nota1 + nota2) / 2
-    print(f"Média final: {media}")
+# def calcNota():
+#     nota1 = int(input("Insira primeira nota: ")) 
+#     nota2 = int(input("Insira segunda nota: ")) 
+#     media = (nota1 + nota2) / 2
+#     print(f"Média final: {media}")
+# 
+# # Exercicio n°1
+# def nome():
+#     nome = input("Digite seu nome: ")
+#     curso = input("Digite o curso: ")
+#     disci = input("Digite a disciplina: ")
+# 
+#     produtos = ["Computador", "Mouse", "Teclado", "Pendrive", "Monitor", "Celular", "HD", "Modem", "Roteador", "Webcam"]
+# 
+#     print(f"{produtos}")
+# 
+#     print(f"Bom dia {nome}")
+#     print(f"Curso: {curso}")
+#     print(f"Disciplina: {disci}\n")
+#     print(f"{produto}")
+# 
+# # Exercicio n°2
+# def cidade():
+#     nome = input("Nome: ")
+#     idade = int(input("Idade: "))
+#     cidade = input("Cidade: ")
+# 
+#     print(f"Nome: {nome}\nIdade: {idade}\nCidade: {cidade}")
+# 
+# # Exercicio n°3
+# def atributos():
+#     altura = 1.80
+#     peso = 73.55
+# 
+#     print(f"{peso}\n{altura}")
+# 
+# # Exercicio n°4
+# def programa4():
+#     idade = 18
+#     ano = 2002
+# 
+# # Exercicio n°5
+# def declarVarComplex():
+#     a = 5+2j
+#     b = 20+6j
+# 
+#     print(a)
+#     print(b)
+# 
+# # Exercicio n°6
+# def declarVarString():
+#     nome = 'Guilherme'
+#     profissao = 'Engenheiro de Software'
+# 
+#     print(f"{profissao}\n{nome}")
+# 
+# # Exercicio n°7
+# def declarVarBool():
+#     fim_de_semana = False
+#     feriado = True 
+# 
+#     print(f"{fim_de_semana}\n{feriado}")
+#     
+# # Exercicio n°8
+# def declarVarList():
+#     alunos = ['Amanda', 'Ana', 'Bruno', 'João']
+#     notas = [10, 8.5, 7.8, 8.0]
+# 
+#     print(f"{alunos}\n{notas}")
 
-# Exercicio n°1
-def nome():
-    nome = input("Digite seu nome: ")
-    curso = input("Digite o curso: ")
-    disci = input("Digite a disciplina: ")
+# Códigos da Aula 1
+# Gabriel Rufino de Souza
+# Prof. Abinael Gomes Barreiros - Programação de Computadores
 
-    produtos = ["Computador", "Mouse", "Teclado", "Pendrive", "Monitor", "Celular", "HD", "Modem", "Roteador", "Webcam"]
+def exercicio1():
+    print("Ciências da Computação - Unicsul")
 
-    print(f"{produtos}")
+def exercicio2():
+    profissao = input("Digite o nome da sua profissão: ")
+    print(f"Profissão: {profissao}")
 
-    print(f"Bom dia {nome}")
-    print(f"Curso: {curso}")
-    print(f"Disciplina: {disci}\n")
-    print(f"{produto}")
+def exercicio3():
+    idade = int(input("Digite a sua idade: "))
+    print(f"Idade: {idade}")
 
-# Exercicio n°2
-def cidade():
-    nome = input("Nome: ")
-    idade = int(input("Idade: "))
-    cidade = input("Cidade: ")
+def exercicio4():
+    family = input("Digite o seu sobrenome: ")
+    print(f"Família {family}")
 
-    print(f"Nome: {nome}\nIdade: {idade}\nCidade: {cidade}")
+def exercicio5():
+    esporte = input("Qual é o seu esporte favorito: ")
+    print(f"Esporte favorito: {esporte}")
 
-# Exercicio n°3
-def atributos():
-    altura = 1.80
-    peso = 73.55
-
-    print(f"{peso}\n{altura}")
-
-# Exercicio n°4
-def programa4():
-    idade = 18
-    ano = 2002
-
-# Exercicio n°5
-def declarVarComplex():
-    a = 5+2j
-    b = 20+6j
-
-    print(a)
-    print(b)
-
-# Exercicio n°6
-def declarVarString():
-    nome = 'Guilherme'
-    profissao = 'Engenheiro de Software'
-
-    print(f"{profissao}\n{nome}")
-
-# Exercicio n°7
-def declarVarBool():
-    fim_de_semana = False
-    feriado = True 
-
-    print(f"{fim_de_semana}\n{feriado}")
-    
-# Exercicio n°8
-def declarVarList():
-    alunos = ['Amanda', 'Ana', 'Bruno', 'João']
-    notas = [10, 8.5, 7.8, 8.0]
-
-    print(f"{alunos}\n{notas}")
-
+exercicio5()
