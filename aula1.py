9c9839600dd04b8c4668961d5d20b537fe7032ac
gabriruf <grufinosouza@tutanota.com>
2025-02-28 08:49:40 -0300

    renomeando arquivo para aula1.py

---
+++ aula1.py
@@ -0,0 +1,94 @@
+# Códigos da Aula 1
+# Gabriel Rufino de Souza
+# Prof. Abinael Gomes Barreiros - Programação de Computadores
+
+# Exercício n°0
+def calcNota():
+    nota1 = int(input("Insira primeira nota: ")) 
+    nota2 = int(input("Insira segunda nota: ")) 
+    media = (nota1 + nota2) / 2
+    print(f"Média final: {media}")
+
+# Exercicio n°1
+def nome():
+    nome = input("Digite seu nome: ")
+    curso = input("Digite o curso: ")
+    disci = input("Digite a disciplina: ")
+
+    produtos = ["Computador", "Mouse", "Teclado", "Pendrive", "Monitor", "Celular", "HD", "Modem", "Roteador", "Webcam"]
+
+    print(f"{produtos}")
+
+    print(f"Bom dia {nome}")
+    print(f"Curso: {curso}")
+    print(f"Disciplina: {disci}\n")
+    print(f"{produto}")
+
+# Exercicio n°2
+def cidade():
+    nome = input("Nome: ")
+    idade = int(input("Idade: "))
+    cidade = input("Cidade: ")
+
+    print(f"Nome: {nome}\nIdade: {idade}\nCidade: {cidade}")
+
+# Exercicio n°3
+def atributos():
+    altura = 1.80
+    peso = 73.55
+
+    print(f"{peso}\n{altura}")
+
+# Exercicio n°4
+def programa4():
+    idade = 18
+    ano = 2002
+
+# Exercicio n°5
+def declarVarComplex():
+    a = 5+2j
+    b = 20+6j
+
+    print(a)
+    print(b)
+
+# Exercicio n°6
+def declarVarString():
+    nome = 'Guilherme'
+    profissao = 'Engenheiro de Software'
+
+    print(f"{profissao}\n{nome}")
+
+# Exercicio n°7
+def declarVarBool():
+    fim_de_semana = False
+    feriado = True 
+
+    print(f"{fim_de_semana}\n{feriado}")
+    
+# Exercicio n°8
+def declarVarList():
+    alunos = ['Amanda', 'Ana', 'Bruno', 'João']
+    notas = [10, 8.5, 7.8, 8.0]
+
+    print(f"{alunos}\n{notas}")
+
+
+def exercicio9():
+    print("Ciências da Computação - Unicsul")
+
+def exercicio10():
+    profissao = input("Digite o nome da sua profissão: ")
+    print(f"Profissão: {profissao}")
+
+def exercicio11():
+    idade = int(input("Digite a sua idade: "))
+    print(f"Idade: {idade}")
+
+def exercicio12():
+    family = input("Digite o seu sobrenome: ")
+    print(f"Família {family}")
+
+def exercicio13():
+    esporte = input("Qual é o seu esporte favorito: ")
+    print(f"Esporte favorito: {esporte}")
